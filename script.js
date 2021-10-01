de648e3cf3221b30ae7c5bd38017a09224286cb3
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-01 19:46:56 +0300

    Добавление переменных, alert, console.log

---
+++ script.js
@@ -0,0 +1,9 @@
+var title = "Java Script";
+var screens = "several";
+var screenPrice = 600;
+var rollback = "default";
+var fullPrice = 1000;
+var adaptive = "always";
+
+alert("My first js code lines");
+console.log("JS is amazing");
