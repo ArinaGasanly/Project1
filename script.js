da7d9f3ce3cdc088836edb122b038cf8598068af
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-03 19:49:02 +0300

    Правки в lesson02 внесены

--- script.js
+++ script.js
@@ -1,71 +1,56 @@
-//1) Следующим переменным присвоить значения
-var title = "Lesson02";
-var screens = ['Простые', 'Сложные', 'Интерактивные'];
-var screenPrice = 756;
-var rollback = 300;
-var fullPrice = 25000;
-var adaptive = true;
+// Следующим переменным присвоить значения
+let title = "Lesson02";
+let screens = ['Простые', 'Сложные', 'Интерактивные'];
+let screenPrice = 756;
+let rollback = 300;
+let fullPrice = 25000;
+let adaptive = true;
+
 
 //Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+
 title = "Lesson02";
 console.log(typeof title);
 
+
 fullPrice = 250000;
 console.log(typeof fullPrice);
+
 
 adaptive = true;
 console.log(typeof adaptive);
 
 // Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-var screens = ['Простые', 'Сложные', 'Интерактивные'];
+let screens = ['Простые', 'Сложные', 'Интерактивные'];
 console.log(screens.length);
 
 //Вывести в консоль “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-var fullPrice = 25000;
-var options1 = { style: 'currency', currency: 'RUB' };
-var numberFormat1 = new Intl.NumberFormat('ru-RU', options1);
-console.log(numberFormat1.format(fullPrice));
 
-var options2 = { style: 'currency', currency: 'USD' };
-var numberFormat2 = new Intl.NumberFormat('en-US', options2);
-console.log(numberFormat2.format(fullPrice));
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'RUB' }).format(fullPrice));
 
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(fullPrice));
 
-var options3 = { style: 'currency', currency: 'UAH' };
-var numberFormat3 = new Intl.NumberFormat('uk-UK', options3);
-console.log(numberFormat3.format(fullPrice));
+console.log(new Intl.NumberFormat('uk-UK', { style: 'currency', currency: 'UAH' }).format(fullPrice));
 
-var options4 = { style: 'currency', currency: 'CNY' };
-var numberFormat4 = new Intl.NumberFormat('zh-ZH', options4);
-console.log(numberFormat4.format(fullPrice));
+console.log(new Intl.NumberFormat('zh-ZH', { style: 'currency', currency: 'CNY' }).format(fullPrice));
+
 
 // “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
-var screenPrice = 756;
-var options1 = { style: 'currency', currency: 'RUB' };
-var numberFormat1 = new Intl.NumberFormat('ru-RU', options1);
-console.log(numberFormat1.format(screenPrice));
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'RUB' }).format(screenPrice));
 
-var options2 = { style: 'currency', currency: 'USD' };
-var numberFormat2 = new Intl.NumberFormat('en-US', options2);
-console.log(numberFormat2.format(screenPrice));
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(screenPrice));
 
+console.log(new Intl.NumberFormat('uk-UK', { style: 'currency', currency: 'UAH' }).format(screenPrice));
 
-var options3 = { style: 'currency', currency: 'UAH' };
-var numberFormat3 = new Intl.NumberFormat('uk-UK', options3);
-console.log(numberFormat3.format(screenPrice));
-
-var options4 = { style: 'currency', currency: 'CNY' };
-var numberFormat4 = new Intl.NumberFormat('zh-ZH', options4);
-console.log(numberFormat4.format(screenPrice));
-
+console.log(new Intl.NumberFormat('zh-ZH', { style: 'currency', currency: 'CNY' }).format(screenPrice));
 
 // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-
-var screens = "Простые', 'Сложные', 'Интерактивные";
+let screens = "Простые', 'Сложные', 'Интерактивные";
 console.log(screens.toLowerCase().split(","));
 
 // Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-var fullPrice = 25000;
-var rollback = 300;
-var result = rollback / 100;
+let fullPrice = 25000;
+let rollback = 300;
+let result = rollback / 100;
 console.log(fullPrice * result);
+
