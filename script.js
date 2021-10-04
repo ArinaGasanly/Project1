ed083ef1202109d84246414903e53f31712fea1a
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-04 20:58:05 +0300

    корректировка вопроса, связанного с дублированием переменных

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-// Следующим переменным присвоить значения
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-
-
 //Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 
-title = "Lesson02";
+let title = "Lesson02";
 console.log(typeof title);
 
-fullPrice = 250000;
+let fullPrice = 250000;
 console.log(typeof fullPrice);
 
-
-adaptive = true;
+let adaptive = true;
 console.log(typeof adaptive);
 
 // Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-screens = ['Простые', 'Сложные', 'Интерактивные'];
+let screens = ['Простые', 'Сложные', 'Интерактивные'];
 console.log(screens.length);
 
 //Вывести в консоль “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” и “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
-screenPrice = 756;
+let screenPrice = 756;
 console.log = ("Стоимость вёрстки экранов " + (fullPrice) + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + (screenPrice) + " RUB / USD / UAH / CNY");
 
 // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
 console.log(screens.toLowerCase().split(","));
 
 // Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-rollback = 300;
+let rollback = 300;
 let result = rollback / 100;
 console.log(fullPrice * result);
 
