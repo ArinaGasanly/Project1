edeb191c1f6bf0689d65c62cc405f320065e6f73
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-05 19:39:09 +0300

    исправление третьего урока

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var title = "Java Script";
-var screens = "several";
-var screenPrice = 600;
-var rollback = "default";
-var fullPrice = 1000;
-var adaptive = "always";
+//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 
-alert("My first js code lines");
-console.log("JS is amazing");
+let title = "Lesson02";
+console.log(typeof title);
 
+let fullPrice = 250000;
+console.log(typeof fullPrice);
+
+let adaptive = true;
+console.log(typeof adaptive);
+
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+let screens = ['Простые', 'Сложные', 'Интерактивные'];
+console.log(screens.length);
+
+//Вывести в консоль “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” и “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
+let screenPrice = 756;
+console.log = ("Стоимость вёрстки экранов " + (fullPrice) + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + (screenPrice) + " RUB / USD / UAH / CNY");
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLowerCase().split(","));
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+let rollback = 300;
+let result = rollback / 100;
+console.log(fullPrice * result);
 
 
 // 3 урок
