63c42f9be736730c2164a18ddc5d72e9d2b8ebd0
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-05 22:22:31 +0300

    удаление информации 4 урок

--- script.js
+++
@@ -1,9 +0,0 @@
-var title = "Java Script";
-var screens = "several";
-var screenPrice = 600;
-var rollback = "default";
-var fullPrice = 1000;
-var adaptive = "always";
-
-alert("My first js code lines");
-console.log("JS is amazing");
