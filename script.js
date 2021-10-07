0cbd846a479d88549a44792aed508cc11bdcc8dd
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-07 20:44:11 +0300

    внесение изменений в третий урок

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 
-let title = "Lesson02";
+let title = "Калькулятор вёрстки";
 console.log(typeof title);
 
 let fullPrice = 250000;
@@ -15,7 +15,7 @@ console.log(screens.length);
 
 //Вывести в консоль “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” и “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
 let screenPrice = 756;
-console.log("Стоимость вёрстки экранов " + Number(fullPrice) + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + Number(screenPrice) + " RUB / USD / UAH / CNY");
+console.log("Стоимость вёрстки экранов " + fullPrice + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + screenPrice + " RUB / USD / UAH / CNY");
 
 // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
 screens = ['Простые', 'Сложные', 'Интерактивные'];
@@ -29,62 +29,57 @@ console.log(fullPrice * result);
 
 // 3 урок
 
-// 2)
+// 3)
 
 title = prompt('Как называется ваш проект?', 'Lesson02');
 console.log('Проект называется ' + title);
-// 3)
+// 4)
 
 screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 console.log('Нужно разрабатывать следующие типы экранов: ' + screens);
 
-// 4)
+// 5)
 
 screenPrice = prompt('Сколько будет стоить данная работа?', 1200);
 console.log(screenPrice);
 
-// 5)
+// 6)
 
 adaptive = confirm("Нужен ли адаптив на сайте?");
 console.log(adaptive);
 
 
-// 6)
+// 7)
 
-let service1 = repair;
-let servicePrice1 = 500;
-let service2 = production;
-let servicePrice2 = 2000;
-
-service1 = prompt('Какой дополнительный тип услуги нужен?', '');
+let service1 = prompt('Какой дополнительный тип услуги нужен?', '');
 console.log(service1);
 
-servicePrice1 = prompt('Сколько это будет стоить?', '');
+let servicePrice1 = prompt('Сколько это будет стоить?', '');
 console.log(servicePrice1);
 
-
-service2 = prompt('Какой дополнительный тип услуги нужен?', '');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', '');
 console.log(service2);
 
-servicePrice2 = prompt('Сколько это будет стоить?', '');
+let servicePrice2 = prompt('Сколько это будет стоить?', '');
 console.log(servicePrice2);
 
-//7)
-
-fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-console.log(fullPrice);
-
 //8)
-let servicePercentPrice = ((fullPrice) / 100) * 81;
-console.log(Math.ceil(servicePercentPrice));
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
 
 //9)
 
-if (fullPrice > 30000) {
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
+console.log(Math.ceil(servicePercentPrice));
+
+//10)
+
+if (fullPrice >= 30000) {
   console.log('Даем скидку в 10%');
-} else if (15000 < fullPrice < 30000) {
+} else if (15000 >= fullPrice && fullPrice < 30000) {
   console.log('Даем скидку в 5%');
-} else if (0 < fullPrice < 15000) {
+} else if (fullPrice >= 0 && fullPrice < 15000) {
   console.log('Скидка не предусмотрена');
 } else {
   console.log('Что-то пошло не так');
