2622f6f4509c0764a5077e254996199b0c89bf33
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-07 22:26:16 +0300

    добавление переменных из 3 урока и корректное указание переменных

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+let rollback = 300;
+let result = rollback / 100;
+let title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = prompt('Сколько будет стоить данная работа?', 756);
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice1 = prompt('Сколько это будет стоить?', '');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice2 = prompt('Сколько это будет стоить?', '');
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
+
+
 // 4 урок
 
 // 1)
-let getAllServicePrices = function () {
+const getAllServicePrices = function () {
   return servicePrice1 + servicePrice2;
 };
 
-let AllServicePrices = getAllServicePrices(servicePrice1, servicePrice1);
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice1);
 
 
 // 2)
 function getFullPrice() {
-  return screenPrice + AllServicePrices;
+  return screenPrice + allServicePrices;
 }
 
-let FullPrice = getFullPrice(screenPrice, AllServicePrices);
+getFullPrice(screenPrice, allServicePrices)
 
 
 // 3)
@@ -31,11 +45,11 @@ fullPrice = 3000;
 function getServicePercentPrices(fullPrice, a, b) {
   return (fullPrice / a) * b;
 }
-let ServicePercentPrices = getServicePercentPrices(fullPrice, 100, 70);
+let servicePercentPrices = getServicePercentPrices(fullPrice, 100, 70);
 
 //5)
 screens = ['Простые', 'Сложные', 'Интерактивные'];
-console.log(String(screens).split(","));
+console.log('Простые, Сложные, Интерактивные'.split(","));
 
 //6)
 
@@ -63,9 +77,22 @@ showTypeOf('Интерактивные')
 showTypeOf('Интерактивные')
 
 console.log(getRollbackMessage(fullPrice));
-
-
-
-
+console.log('Проект называется ' + title);
+console.log('Нужно разрабатывать следующие типы экранов: ' + screens);
+console.log(screenPrice);
+console.log(adaptive);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость вёрстки экранов " + fullPrice + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + screenPrice + " RUB / USD / UAH / CNY");
+console.log('Простые, Сложные, Интерактивные'.toLowerCase().split(","));
+console.log(fullPrice);
+console.log(fullPrice * result);
+console.log(Math.ceil(servicePercentPrice));
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
 
 
