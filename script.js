ea017bdddbd866e7010a74bd3fc5e2a1863f191e
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-07 20:15:04 +0300

    добавление 5 урока

--- script.js
+++ script.js
@@ -1,93 +1,122 @@
-//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+// 5 урок
 
-let title = "Lesson02";
+let title
+let screens
+let screenPrice
+let adaptive
+let rollback = 10;
+let allServicePrices
+let fullPrice
+let servicePercentPrice
+let service1
+let service2
+
+
+//5 урок
+ 
+ let AllServicePrices = 4000
+ 
+  do {
+    screenPrice = +prompt('Сколько будет стоить данная работа?', 2000);
+
+  } while (screenPrice === allServicePrices); {
+
+
+  }
+
+
+
+const isNumber = function(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+
+const asking = function() {
+  title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
+
+  screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные')
+
+  screenPrice = prompt('Сколько будет стоить данная работа?')
+
+  while(isNumber(screenPrice) || screenPrice.trim() === "" || screenPrice === null) {
+    screenPrice = prompt('Сколько будет стоить данная работа?')
+  }
+
+  adaptive = confirm('Нужен ли адаптив на сайте?')
+}
+
+
+const getAllServicePrices = function () {
+  let sum = 0
+
+  for (let i = 0; i < 2; i++) {
+
+    if (i === 0) {
+      service1 = prompt('Какой дополнительный тип услуги нужен?')
+
+    } else if (i === 1) {
+      service2 = prompt('Какой дополнительный тип услуги нужен?')
+
+    }
+
+
+    sum += prompt('Сколько это будет стоить?')
+  }
+
+  return sum
+  //return servicePrice1 + servicePrice2
+}
+
+const showTypeOf = function (screens) {
+  console.log('Простые, Сложные, Интерактивные'.split(","));
+}
+
+const getFullPrice = function () {
+  return screenPrice + AllServicePrices
+}
+
+const getServicePercentPrices = function () {
+  return fullPrice - (fullPrice * (rollback / 100))
+
+}
+
+const getTitle = function() {
+  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
+}
+
+
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return 'Даем скидку в 10%'
+  } else if (price >= 15000 && price < 30000) {
+    return 'Даем скидку в 5%'
+  } else if (price >= 0 && price < 15000) {
+    return 'Скидка не предусмотрена'
+  } else {
+    return 'Что-то пошло не так'
+  }
+}
+asking()
+allServiceprices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrice()
+title = getTitle()
+
+
+showTypeOf('Простые')
+showTypeOf('Сложные')
+showTypeOf('Интерактивные')
+
+console.log('allServicePrices', allServicePrices);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
-
-let fullPrice = 250000;
-console.log(typeof fullPrice);
-
-let adaptive = true;
+console.log(typeof screenPrice);
 console.log(typeof adaptive);
 
-// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-let screens = ['Простые', 'Сложные', 'Интерактивные'];
 console.log(screens.length);
+console.log(servicePercentPrice);
 
-//Вывести в консоль “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” и “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
-let screenPrice = 756;
-console.log("Стоимость вёрстки экранов " + Number(fullPrice) + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + Number(screenPrice) + " RUB / USD / UAH / CNY");
-
-// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-screens = ['Простые', 'Сложные', 'Интерактивные'];
-console.log('Простые, Сложные, Интерактивные'.toLowerCase().split(","));
-
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-let rollback = 300;
-let result = rollback / 100;
-console.log(fullPrice * result);
-
-
-// 3 урок
-
-// 2)
-
-title = prompt('Как называется ваш проект?', 'Lesson02');
-console.log('Проект называется ' + title);
-// 3)
-
-screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-console.log('Нужно разрабатывать следующие типы экранов: ' + screens);
-
-// 4)
-
-screenPrice = prompt('Сколько будет стоить данная работа?', 1200);
-console.log(screenPrice);
-
-// 5)
-
-adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptive);
-
-
-// 6)
-
-let service1 = repair;
-let servicePrice1 = 500;
-let service2 = production;
-let servicePrice2 = 2000;
-
-service1 = prompt('Какой дополнительный тип услуги нужен?', '');
-console.log(service1);
-
-servicePrice1 = prompt('Сколько это будет стоить?', '');
-console.log(servicePrice1);
-
-
-service2 = prompt('Какой дополнительный тип услуги нужен?', '');
-console.log(service2);
-
-servicePrice2 = prompt('Сколько это будет стоить?', '');
-console.log(servicePrice2);
-
-//7)
-
-fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-console.log(fullPrice);
-
-//8)
-let servicePercentPrice = ((fullPrice) / 100) * 81;
-console.log(Math.ceil(servicePercentPrice));
-
-//9)
-
-if (fullPrice > 30000) {
-  console.log('Даем скидку в 10%');
-} else if (15000 < fullPrice < 30000) {
-  console.log('Даем скидку в 5%');
-} else if (0 < fullPrice < 15000) {
-  console.log('Скидка не предусмотрена');
-} else {
-  console.log('Что-то пошло не так');
-}
+console.log("Стоимость вёрстки экранов " + fullPrice + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + screenPrice + " RUB / USD / UAH / CNY";
 
 
