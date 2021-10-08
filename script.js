3f7e180d00e7480db2f18f832393cceb4a3b5853
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-08 23:48:05 +0300

    добавление пятого задания

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 // 5 урок
 
-let title
-let screens
-let screenPrice
-let adaptive
-let rollback = 10;
-let allServicePrices
-let fullPrice
-let servicePercentPrice
-let service1
-let service2
+let rollback = 30;
+let title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = prompt('Сколько будет стоить данная работа?', 756);
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice1 = prompt('Сколько это будет стоить?', '');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice2 = prompt('Сколько это будет стоить?', '');
+let fullPrice = 3000;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
 
 
-//5 урок
- 
- let AllServicePrices = 4000
+//1 задание
+
+const allServicePrices = prompt('Сколько это будет стоить?', '4000');
  
   do {
     screenPrice = +prompt('Сколько будет стоить данная работа?', 2000);
 
-  } while (screenPrice === allServicePrices); {
-
-
+  } while (typeof screenPrice == 'number' && typeof allServicePrices == 'number') {
+      const getAllServicePrices = function () {
+        return prompt('Сколько это будет стоить?');
+  
+   } 
   }
 
+  screenPrice();
 
+
+
+
+
+
+
+  // Практическая часть (видео)
 
 const isNumber = function(num) {
   return !isNaN(parseFloat(num)) && isFinite(num)
@@ -66,57 +77,4 @@ const getAllServicePrices = function () {
   return sum
   //return servicePrice1 + servicePrice2
 }
-
-const showTypeOf = function (screens) {
-  console.log('Простые, Сложные, Интерактивные'.split(","));
-}
-
-const getFullPrice = function () {
-  return screenPrice + AllServicePrices
-}
-
-const getServicePercentPrices = function () {
-  return fullPrice - (fullPrice * (rollback / 100))
-
-}
-
-const getTitle = function() {
-  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
-}
-
-
-const getRollbackMessage = function (price) {
-  if (price >= 30000) {
-    return 'Даем скидку в 10%'
-  } else if (price >= 15000 && price < 30000) {
-    return 'Даем скидку в 5%'
-  } else if (price >= 0 && price < 15000) {
-    return 'Скидка не предусмотрена'
-  } else {
-    return 'Что-то пошло не так'
-  }
-}
-asking()
-allServiceprices = getAllServicePrices()
-fullPrice = getFullPrice()
-servicePercentPrice = getServicePercentPrice()
-title = getTitle()
-
-
-showTypeOf('Простые')
-showTypeOf('Сложные')
-showTypeOf('Интерактивные')
-
-console.log('allServicePrices', allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(servicePercentPrice);
-
-console.log("Стоимость вёрстки экранов " + fullPrice + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + screenPrice + " RUB / USD / UAH / CNY";
-
 
