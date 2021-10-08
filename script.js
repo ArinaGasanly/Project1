a3d596b2730210f8efe3dce4e6a0e8c8ad40a4c3
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-08 20:19:41 +0300

    внесение корректировок по 4 уроку

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let rollback = 300;
-let result = rollback / 100;
+let rollback = 30;
 let title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let screenPrice = prompt('Сколько будет стоить данная работа?', 756);
@@ -8,18 +7,19 @@ let service1 = prompt('Какой дополнительный тип услуг
 let servicePrice1 = prompt('Сколько это будет стоить?', '');
 let service2 = prompt('Какой дополнительный тип услуги нужен?', '');
 let servicePrice2 = prompt('Сколько это будет стоить?', '');
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = 3000;
 let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
 
 
 // 4 урок
 
 // 1)
+
 const getAllServicePrices = function () {
   return servicePrice1 + servicePrice2;
 };
 
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice1);
+const allServicePrices = getAllServicePrices();
 
 
 // 2)
@@ -27,37 +27,39 @@ function getFullPrice() {
   return screenPrice + allServicePrices;
 }
 
-getFullPrice(screenPrice, allServicePrices)
+getFullPrice();
 
 
 // 3)
 let str = 'title';
 
-function getTitle() {
-  return str.split("").map((s, i) => (i % 2 != 0 ? s.toUpperCase() : s)).join("");
+function getTitle(title) {
+  return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
 }
-
-getTitle('title');
+title = getTitle(title);
 
 // 4)
 
-fullPrice = 3000;
-function getServicePercentPrices(fullPrice, a, b) {
-  return (fullPrice / a) * b;
+
+function getServicePercentPrices() {
+  return fullPrice - (fullPrice * (rollback / 100));
 }
-let servicePercentPrices = getServicePercentPrices(fullPrice, 100, 70);
+const servicePercentPrices = getServicePercentPrices();
 
 //5)
-screens = ['Простые', 'Сложные', 'Интерактивные'];
 console.log('Простые, Сложные, Интерактивные'.split(","));
+
 
 //6)
 
 
-const showTypeOf = function(screens) {
-  console.log(String(screens).split(","));
+const showTypeOf = function(data) {
+  console.log('Тип экранов' + data + ' является: ' + typeof data);
 }
 
+showTypeOf(' Простые ')
+showTypeOf(' Интерактивные ')
+showTypeOf(' Сложные ')
 
 
 const getRollbackMessage = function(price) {
@@ -72,27 +74,8 @@ const getRollbackMessage = function(price) {
   }
 }
 
-showTypeOf('Интерактивные')
-showTypeOf('Интерактивные')
-showTypeOf('Интерактивные')
+
 
 console.log(getRollbackMessage(fullPrice));
-console.log('Проект называется ' + title);
-console.log('Нужно разрабатывать следующие типы экранов: ' + screens);
-console.log(screenPrice);
-console.log(adaptive);
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость вёрстки экранов " + fullPrice + " RUB / USD / UAH / CNY" + " и Стоимость разработки сайта " + screenPrice + " RUB / USD / UAH / CNY");
-console.log('Простые, Сложные, Интерактивные'.toLowerCase().split(","));
-console.log(fullPrice);
-console.log(fullPrice * result);
-console.log(Math.ceil(servicePercentPrice));
-console.log(service1);
-console.log(servicePrice1);
-console.log(service2);
-console.log(servicePrice2);
 
 
